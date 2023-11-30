36335a45fc5bdaa5d5699262fbb848381f50136a
Harsh <harshrastogi396@gmail.com>
2023-11-30 11:41:16 +0530

    instance created successfull

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import axios from './Redux/axios';
-axios.interceptors.request.use(config=>{
-    console.log(config);
-  })
-  axios.interceptors.response.use(response=>{
-    console.log(response);
-    return response
-  })
+// axios.interceptors.request.use(config=>{
+//     console.log(config);
+//   })
+//   axios.interceptors.response.use(response=>{
+//     console.log(response);
+//     return response
+//   })
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
